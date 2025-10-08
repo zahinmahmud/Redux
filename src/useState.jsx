24986766251850ca3f337fc885efd9dd7c10caec
zahinmahmud <66882223+zahinmahmud@users.noreply.github.com>
2025-10-08 14:56:54 +0600

    Create useState.jsx

---
+++ src/useState.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+
+function Counter() {
+  const [count, setCount] = useState(0); // initialize state
+
+  return (
+    <div>
+      <h2>Count: {count}</h2>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+      <button onClick={() => setCount(count - 1)}>Decrement</button>
+    </div>
+  );
+}
+
+export default Counter;
