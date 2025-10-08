b64f0f91eaa3127d0af79b8571065f927ad17553
zahinmahmud <66882223+zahinmahmud@users.noreply.github.com>
2025-10-08 14:54:26 +0600

    Create ClBasHook.jsx

---
+++ src/ClBasHook.jsx
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { count: 0 }; // state initialization
+  }
+
+  increment = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
+
+  decrement = () => {
+    this.setState({ count: this.state.count - 1 });
+  };
+
+  render() {
+    return (
+      <div>
+        <h2>Count: {this.state.count}</h2>
+        <button onClick={this.increment}>Increment</button>
+        <button onClick={this.decrement}>Decrement</button>
+      </div>
+    );
+  }
+}
+
+export default Counter;
