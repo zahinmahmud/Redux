f67ca305c31fd354a6bd0b018947d2f1aefc20c9
zahinmahmud <66882223+zahinmahmud@users.noreply.github.com>
2025-10-08 15:02:02 +0600

    Create useReducer.jsx

---
+++ src/useReducer.jsx
@@ -0,0 +1,28 @@
+import React, { useReducer } from "react";
+
+const initialState = { count: 0 };
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "increment":
+      return { count: state.count + 1 };
+    case "decrement":
+      return { count: state.count - 1 };
+    default:
+      return state;
+  }
+}
+
+function Counter() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  return (
+    <div>
+      <h2>Count: {state.count}</h2>
+      <button onClick={() => dispatch({ type: "increment" })}>+</button>
+      <button onClick={() => dispatch({ type: "decrement" })}>-</button>
+    </div>
+  );
+}
+
+export default Counter;
